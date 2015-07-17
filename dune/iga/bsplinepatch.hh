d73ffd2fda8c2e89249c955b3dd8b73d50fedb72
Eugen <Eugen.Salzmann@rwth-aachen.de>
2015-07-17 08:53:16 +0200

    working bsplinecurves

--- dune/iga/bsplinepatch.hh
+++ dune/iga/bsplinepatch.hh
@@ -2,27 +2,125 @@
 #define DUNE_IGA_BSPLINEPATCH_HH
 
 #include <dune/geometry/multilineargeometry.hh>
+#include <memory>
 
 namespace Dune
 {
 namespace IGA
 {
 
+template<int dim, int dimworld>
+class BsplinePatchData
+{
+public:
+    BsplinePatchData(const std::array<std::vector<double>,dim>& knotSpans,
+               const std::vector<FieldVector<double,dimworld> > controlPoints,
+               const int order)
+    : knotSpans_(knotSpans)
+    , controlPoints_(controlPoints)
+    , order_(order)
+    {
+    }
+
+    const std::array<std::vector<double>, dim> & getknots()
+    {
+        return knotSpans_;
+    }
+    const std::vector<FieldVector<double,dimworld>> & getcontrolPoints ()
+    {
+        return controlPoints_;
+    }
+    const int getorder()
+    {
+        return order_;
+    }
+private:
+
+  const std::array<std::vector<double>,dim>& knotSpans_;
+
+  std::vector<FieldVector<double,dimworld> > controlPoints_;
+
+  const int order_;
+
+};
+
 template <int dim, int dimworld>
 class BSplineGeometry
 {
 public:
 
-  BSplineGeometry(const std::vector<FieldVector<double,dimworld> >& corners)
-  : multiLinearGeometry_(GeometryType(GeometryType::BasicType::cube,dim), corners)
+  BSplineGeometry(std::shared_ptr <BsplinePatchData<dim,dimworld>> Patchdata, const double* corner)
+  : Patchdata_(Patchdata)
+  , corner_(corner)
   {}
 
   FieldVector<double,dimworld> global(const FieldVector<double,dim>& local) const
   {
-    return multiLinearGeometry_.global(local);
+    if (local[0]<0 || local[0]>1)
+    {
+        //DUNE_THROW(OutofRange, "local coordinates havae to be in [0,1]!");
+        std::cout<<"fail"<<std::endl;
+    }
+    else
+    {
+        double loc = local[0]*(*(corner_+1)-*corner_) + *corner_;
+
+        const auto& knotSpans = Patchdata_->getknots();
+        const auto& controlPoints = Patchdata_->getcontrolPoints();
+        auto order = Patchdata_->getorder();
+
+        std::vector<std::vector<double>> basis;
+        basis.resize(order+1);
+
+        for (int j=0; j<knotSpans[0].size()-1; ++j)
+        {
+            if(&(knotSpans[0][j]) == (corner_))
+                basis[0].push_back(1);
+            else
+            {
+                basis[0].push_back(0);
+            }
+        }
+
+         double A,B;
+        for (int o=1; o<=order; ++o)
+        {
+            for (int k=0; k<(knotSpans[0].size()-o); ++k)
+            {
+                if ((knotSpans[0][k+o]-knotSpans[0][k]) == 0)
+                    A = 0;
+                else
+                    A = ((loc)-knotSpans[0][k])/ (knotSpans[0][k+o]-knotSpans[0][k]);
+
+                if ((knotSpans[0][k+o+1]-knotSpans[0][k+1]) == 0)
+                    B = 0;
+                else
+                    B = (knotSpans[0][k+o+1]-(loc))/ (knotSpans[0][k+o+1]-knotSpans[0][k+1]);
+
+                 basis[o].push_back(A*basis[o-1][k] + B*basis[o-1][k+1]);
+            }
+        }
+
+        FieldVector<double,dimworld> glob = {0,0,0};
+
+        for (int i=0; i<controlPoints.size(); ++i)
+        {
+            for (int j=0; j<dimworld; ++j)
+                glob[j] += controlPoints[i][j]*basis[order][i];
+
+        }
+
+    return glob;
+
+    }
   }
 
-  MultiLinearGeometry<double,dim,dimworld> multiLinearGeometry_;
+private:
+
+    std::shared_ptr <BsplinePatchData<dim,dimworld>> Patchdata_;
+
+    const double* corner_;
+
 };
 
 template <int dim, int dimworld>
@@ -30,51 +128,74 @@ class BSplinePatch
 {
 public:
 
-  BSplinePatch(const FieldVector<double,dim>& lower,
-               const FieldVector<double,dim>& upper,
-               const std::array<unsigned int,dim>& knotSpans,
-               const std::vector<FieldVector<double,dimworld> > controlPoints)
-  : knotSpans_(knotSpans),
-    controlPoints_(controlPoints)
+  BSplinePatch(/*const FieldVector<double,dim>& lower,
+               const FieldVector<double,dim>& upper,*/
+               const std::array<std::vector<double>,dim>& knotSpans,
+               const std::vector<FieldVector<double,dimworld> > controlPoints,
+               const int order)
+  : Patchdata_(new BsplinePatchData<dim,dimworld>(knotSpans, controlPoints, order))
   {
-
   }
 
-  BSplineGeometry<dim,dimworld> geometry(const std::array<unsigned int,dim>& ijk) const
+  BSplineGeometry<dim,dimworld> geometry(const std::array<unsigned int,dim>& ijk ) const
   {
-    std::vector<FieldVector<double,dimworld> > corners(1<<dim);
+//     std::vector<FieldVector<double,dimworld> > corners(1<<dim);
+//         corners[0] = controlPoints_[ijk[0]];
+//         corners[1] = controlPoints_[ijk[0]+1];
 
-    switch (dim)
-    {
-      case 1:
-      {
-        corners[0] = controlPoints_[ijk[0]];
-        corners[1] = controlPoints_[ijk[0]+1];
+//     switch (dim)
+//     {
+//       case 1:
+//       {
+//         corners[0] = controlPoints_[ijk[0]];
+//         corners[1] = controlPoints_[ijk[0]+1];
+//
+//         break;
+//       }
+//       case 2:
+//       {
+//         corners[0] = controlPoints_[ijk[1]*(knotSpans_[0]+1) + ijk[0]];
+//         corners[1] = controlPoints_[ijk[1]*(knotSpans_[0]+1) + ijk[0]+1];
+//         corners[2] = controlPoints_[(ijk[1]+1)*(knotSpans_[0]+1) + ijk[0]];
+//         corners[3] = controlPoints_[(ijk[1]+1)*(knotSpans_[0]+1) + ijk[0]+1];
+//
+//         break;
+//       }
+//       default:
+//         DUNE_THROW(Dune::NotImplemented, "General case not implemented yet!");
+//     }
 
-        break;
-      }
-      case 2:
-      {
-        corners[0] = controlPoints_[ijk[1]*(knotSpans_[0]+1) + ijk[0]];
-        corners[1] = controlPoints_[ijk[1]*(knotSpans_[0]+1) + ijk[0]+1];
-        corners[2] = controlPoints_[(ijk[1]+1)*(knotSpans_[0]+1) + ijk[0]];
-        corners[3] = controlPoints_[(ijk[1]+1)*(knotSpans_[0]+1) + ijk[0]+1];
 
-        break;
-      }
-      default:
-        DUNE_THROW(Dune::NotImplemented, "General case not implemented yet!");
-    }
-    return BSplineGeometry<dim,dimworld>(corners);
+        unsigned int i = 0;
+        unsigned int count = 0;
+
+        auto& knotSpans = Patchdata_->getknots();
+
+
+        for (int j=0; j<dim; ++j)
+        {
+            while(count <= ijk[0])
+            {
+                if (i == knotSpans[0].size())
+                    break;
+
+                if (knotSpans[j][i+1] > knotSpans[j][i])
+                    count++;
+
+                ++i;
+            }
+
+        }
+
+    return BSplineGeometry<dim,dimworld>(Patchdata_,&knotSpans[0][i-1]);
   }
 
 private:
 
-  const std::array<unsigned int,dim>& knotSpans_;
-
-  std::vector<FieldVector<double,dimworld> > controlPoints_;
+  std::shared_ptr <BsplinePatchData<dim,dimworld>> Patchdata_;
 
 };
+
 
 }
 
--- src/dune-iga.cc
+++ src/dune-iga.cc
@@ -4,104 +4,108 @@
 
 #include <iostream>
 
+
 #include <dune/common/parallel/mpihelper.hh>
 #include <dune/common/exceptions.hh>
 #include <dune/common/fvector.hh>
 
 #include <dune/iga/bsplinepatch.hh>
+//#include "../dune/iga/bsplinepatch.hh"
 #include <dune/iga/vtkfile.hh>
-
+//#include "../dune/iga/vtkfile.hh"
 using namespace Dune;
 
-void testBSplineSurface()
-{
-  // parameters
-  unsigned int subSampling = 2;
-
-  ////////////////////////////////////////////////////////////////
-  //  Create a 2d B-spline surface in 3d
-  ////////////////////////////////////////////////////////////////
-
-  const int dim      = 2;
-  const int dimworld = 3;
-
-  FieldVector<double,dim> lower = {0.0, 0.0};
-  FieldVector<double,dim> upper = {1.0, 1.0};
-
-  std::array<unsigned int,dim> knotSpans = {2, 2};
-
-  // very simple geometry: a square
-  std::vector<FieldVector<double,dimworld> > controlPoints = {{0,0,0},    {1,0,0.25}, {2,0,0},
-                                                              {0,1,0.25}, {1,1,0.5},  {2,1,0.25},
-                                                              {0,2,0},    {1,2,0.25}, {2,2,0}};
-
-  IGA::BSplinePatch<dim,dimworld> patch(lower, upper, knotSpans, controlPoints);
-
-  ////////////////////////////////////////////////////////////////
-  //  Write to a VTK file.
-  //  The higher-order geometry is captured by subsampling.
-  ////////////////////////////////////////////////////////////////
-
-  IGA::VTKFile vtkFile;
-
-  //  The number of vertices that have been inserted so far
-  std::size_t offset = 0;
-
-  for (unsigned int i=0; i<knotSpans[1]; i++)
-  {
-    for (unsigned int j=0; j<knotSpans[0]; j++)
-    {
-      auto geometry = patch.geometry({j,i});
-
-      // Add vertex coordinates to the VTK file
-      for (int iy=0; iy<=(1<<subSampling); iy++)
-      {
-        FieldVector<double,dim> localPos;
-        localPos[1] = ((double)iy)/(1<<subSampling);
-
-        // Add vertex coordinates to the VTK file
-        for (int ix=0; ix<=(1<<subSampling); ix++)
-        {
-          localPos[0] = ((double)ix)/(1<<subSampling);
-
-          vtkFile.points_.push_back(geometry.global(localPos));
-        }
-      }
-
-      // Add elements to the VTK file
-      for (int k=0; k<(1<<subSampling); k++)
-      {
-        for (int l=0; l<(1<<subSampling); l++)
-        {
-          vtkFile.cellConnectivity_.push_back(offset + k    *((1<<subSampling)+1) + l);
-          vtkFile.cellConnectivity_.push_back(offset + k    *((1<<subSampling)+1) + l+1);
-          vtkFile.cellConnectivity_.push_back(offset + (k+1)*((1<<subSampling)+1) + l+1);
-          vtkFile.cellConnectivity_.push_back(offset + (k+1)*((1<<subSampling)+1) + l);
-
-          // 4 corners per element
-          if (vtkFile.cellOffsets_.size()==0)
-            vtkFile.cellOffsets_.push_back(4);
-          else
-            vtkFile.cellOffsets_.push_back(vtkFile.cellOffsets_.back()+4);
-
-          // Element type: a 4-node quadrilateral
-          vtkFile.cellTypes_.push_back(9);
-        }
-      }
-
-      offset += ((1<<subSampling)+1) * ((1<<subSampling)+1);
-    }
-  }
-
-  // Actually write the VTK file
-  vtkFile.write("bsplinesurface");
-}
+// void testBSplineSurface()
+// {
+//   // parameters
+//   unsigned int subSampling = 2;
+//
+//   ////////////////////////////////////////////////////////////////
+//   //  Create a 2d B-spline surface in 3d
+//   ////////////////////////////////////////////////////////////////
+//
+//   const int dim      = 2;
+//   const int dimworld = 3;
+//   const int order    = 1;
+//
+//   FieldVector<double,dim> lower = {0.0, 0.0};
+//   FieldVector<double,dim> upper = {1.0, 1.0};
+//
+//   std::array<unsigned int,dim> knotSpans = {2, 2};
+//   std::cout<<knotSpans<<std::endl;
+//
+//   // very simple geometry: a square
+//   std::vector<FieldVector<double,dimworld> > controlPoints = {{0,0,0},    {1,0,0.25}, {2,0,0},
+//                                                               {0,1,0.25}, {1,1,0.5},  {2,1,0.25},
+//                                                               {0,2,0},    {1,2,0.25}, {2,2,0}};
+//
+//   IGA::BSplinePatch<dim,dimworld> patch(lower, upper, knotSpans, controlPoints, order);
+//
+//   ////////////////////////////////////////////////////////////////
+//   //  Write to a VTK file.
+//   //  The higher-order geometry is captured by subsampling.
+//   ////////////////////////////////////////////////////////////////
+//
+//   IGA::VTKFile vtkFile;
+//
+//   //  The number of vertices that have been inserted so far
+//   std::size_t offset = 0;
+//
+//   for (unsigned int i=0; i<knotSpans[1]; i++)
+//   {
+//     for (unsigned int j=0; j<knotSpans[0]; j++)
+//     {
+//       auto geometry = patch.geometry({j,i});
+//
+//       // Add vertex coordinates to the VTK file
+//       for (int iy=0; iy<=(1<<subSampling); iy++)
+//       {
+//         FieldVector<double,dim> localPos;
+//         localPos[1] = ((double)iy)/(1<<subSampling);
+//
+//         // Add vertex coordinates to the VTK file
+//         for (int ix=0; ix<=(1<<subSampling); ix++)
+//         {
+//           localPos[0] = ((double)ix)/(1<<subSampling);
+//
+//           vtkFile.points_.push_back(geometry.global(localPos));
+//         }
+//       }
+//
+//       // Add elements to the VTK file
+//       for (int k=0; k<(1<<subSampling); k++)
+//       {
+//         for (int l=0; l<(1<<subSampling); l++)
+//         {
+//           vtkFile.cellConnectivity_.push_back(offset + k    *((1<<subSampling)+1) + l);
+//           vtkFile.cellConnectivity_.push_back(offset + k    *((1<<subSampling)+1) + l+1);
+//           vtkFile.cellConnectivity_.push_back(offset + (k+1)*((1<<subSampling)+1) + l+1);
+//           vtkFile.cellConnectivity_.push_back(offset + (k+1)*((1<<subSampling)+1) + l);
+//
+//           // 4 corners per element
+//           if (vtkFile.cellOffsets_.size()==0)
+//             vtkFile.cellOffsets_.push_back(4);
+//           else
+//             vtkFile.cellOffsets_.push_back(vtkFile.cellOffsets_.back()+4);
+//
+//           // Element type: a 4-node quadrilateral
+//           vtkFile.cellTypes_.push_back(9);
+//         }
+//       }
+//
+//       offset += ((1<<subSampling)+1) * ((1<<subSampling)+1);
+//     }
+//   }
+//
+//   // Actually write the VTK file
+//   vtkFile.write("bsplinesurface");
+// }
 
 
 void testBSplineCurve()
 {
   // parameters
-  unsigned int subSampling = 2;
+  unsigned int subSampling = 5;
 
   ////////////////////////////////////////////////////////////////
   //  Create a B-spline curve in 3d
@@ -109,27 +113,32 @@ void testBSplineCurve()
 
   const int dim      = 1;
   const int dimworld = 3;
+  const int order    = 2;
 
-  FieldVector<double,dim> lower = {0.0};
-  FieldVector<double,dim> upper = {1.0};
+//   FieldVector<double,dim> lower = {0.0};
+//   FieldVector<double,dim> upper = {1.0};
 
-  std::array<unsigned int,dim> knotSpans = {2};
+  std::vector<double> xspans={0,0,0,1,1,2,3,4,4,5,5,5};
+  //std::vector<double> xspans={0,1,2,3,4,5,6,7,8,9,10};
+  std::array<std::vector<double>,dim> knotSpans = {xspans};
 
-  std::vector<FieldVector<double,dimworld> > controlPoints = {{0,0,0}, {1,0.25,0}, {2,0,0}};
+  std::vector<FieldVector<double,dimworld> > controlPoints = {{1,3,4}, {2,2,2}, {3,4,5}, {5,1,7}, {4,7,2}, {8,6,2}, {2,9,9}, {1,4,3},{1,7,1}};
 
-  IGA::BSplinePatch<dim,dimworld> patch(lower, upper, knotSpans, controlPoints);
+  IGA::BSplinePatch<dim,dimworld> patch(/*lower, upper, */knotSpans, controlPoints, order);
 
   ////////////////////////////////////////////////////////////////
-  //  Write to a VTK file.
-  //  The higher-order geometry is captured by subsampling.
+  //  Write to a VTK file.                                      //
+  //  The higher-order geometry is captured by subsampling.     //
   ////////////////////////////////////////////////////////////////
 
-  IGA::VTKFile vtkFile;
+ IGA::VTKFile vtkFile;
 
-  //  The number of vertices that have been inserted so far
-  std::size_t offset = 0;
+ //  The number of vertices that have been inserted so far
+ std::size_t offset = 0;
 
-  for (unsigned int i=0; i<knotSpans[0]; i++)
+ int max = knotSpans[0][knotSpans[0].size()-1] - knotSpans[0][0];
+
+  for (unsigned int i=0; i<max; i++)
   {
     auto geometry = patch.geometry({i});
 
@@ -139,7 +148,6 @@ void testBSplineCurve()
     for (int ix=0; ix<=(1<<subSampling); ix++)
     {
       localPos[0] = ((double)ix)/(1<<subSampling);
-
       vtkFile.points_.push_back(geometry.global(localPos));
     }
 
@@ -169,11 +177,18 @@ void testBSplineCurve()
 
 int main(int argc, char** argv) try
 {
+
   // Initialize MPI, if necessary
   MPIHelper::instance(argc, argv);
+  const int dim =1;
+
 
   testBSplineCurve();
-  testBSplineSurface();
+  //testBSplineSurface();
+
+   std::cout<< "run" << std::endl;
+
+
 
   return 0;
 }
