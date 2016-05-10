6d4bc95a992319870f903281060b4adc60787be5
Xinyun Li <youround516@gmail.com>
2016-05-10 22:54:01 +0200

    Add function for computing Jacobian matrix and intergrationElement, still working on the inverse

--- dune/iga/bsplinepatch.hh
+++ dune/iga/bsplinepatch.hh
@@ -5,6 +5,10 @@
 
 #include <memory>
 #include <dune/geometry/type.hh>
+#include <dune/common/fmatrix.hh>
+#include <dune/common/fvector.hh>
+#include <dune/geometry/genericgeometry/geometrytraits.hh>
+#include <dune/geometry/genericgeometry/matrixhelper.hh>
 
 namespace Dune
 {
@@ -217,6 +221,16 @@ namespace Dune
       /** \brief Type used for a vector of world coordinates */
       typedef FieldVector<ctype,dimworld> GlobalCoordinate;
 
+      /** \brief Type for the transposed Jacobian matrix */
+      typedef FieldMatrix< ctype, mydimension, coorddimension > JacobianTransposed;
+
+      /** \brief Type for the transposed inverse Jacobian matrix */
+      typedef FieldMatrix< ctype, coorddimension, mydimension > JacobianInverseTransposed;
+    private:
+      /* Helper class to compute a matrix pseudo inverse */
+      typedef GenericGeometry::MatrixHelper< GenericGeometry::DuneCoordTraits< double > > MatrixHelper;
+
+    public:
       /** \brief Constructor of BSplineGeometry from BSplinePatchData and an iterator to a specific knot
        *
        *  \param[in] Patchdata shared pointer to an object where the all the data of the BSplinePatch is stored
@@ -227,7 +241,7 @@ namespace Dune
       , corner_(corner)
       {
         // note it`s maybe not such a good a idea to store all of that!
-        std::array<std::vector<double>,dim> _basis;
+        std::array<std::vector<std::vector<double>>,dim> _basis;
       }
 
       /** \brief Map the center of the element to the geometry */
@@ -241,14 +255,14 @@ namespace Dune
       /** \brief Return the number of corners of the element */
       int corners() const
       {
-        return 1<<dim;
+        return 1<<mydimension;
       }
 
       /** \brief Return world coordinates of the k-th corner of the element */
       GlobalCoordinate corner(int k) const
       {
         LocalCoordinate localcorner;
-        for (size_t i=0; i<dim; i++) {
+        for (size_t i=0; i<mydimension; i++) {
             localcorner[i] = (k & (1<<i)) ? 0 : 1;
         }
         return global(localcorner);
@@ -267,9 +281,8 @@ namespace Dune
        *
        *  \param[in] local local coordinates for each dimension
        */
-      GlobalCoordinate global(const LocalCoordinate& local)
+      const GlobalCoordinate global(const LocalCoordinate& local)
       {
-
         const auto& knotSpans = patchData_->getKnots();
         const auto& controlPoints = patchData_->getControlPoints();
         const auto& order = patchData_->getOrder();
@@ -277,7 +290,6 @@ namespace Dune
         GlobalCoordinate glob;
         std::fill(glob.begin(), glob.end(), 0.0);
         std::array<unsigned int,dim> multiIndexBasisfucntion, multiIndexControlNet;
-        double temp;
         std::array<unsigned int,dim> dimsize;
         std::array<unsigned int,dim> cornerIdx;
         for (unsigned int d=0; d<dim; ++d)
@@ -290,10 +302,10 @@ namespace Dune
         for (unsigned int i=0; i<basisFunctionNet.directSize(); ++i)
         {
           multiIndexBasisfucntion =  basisFunctionNet.directToMultiIndex(i);
-          temp = 1;
+          double temp = 1;
           for (unsigned int d=0; d<dim; ++d)
           {
-              temp *= basis[d][multiIndexBasisfucntion[d]];
+              temp *= basis[d][multiIndexBasisfucntion[d]][order[d]];
               multiIndexControlNet[d] = multiIndexBasisfucntion[d]+cornerIdx[d]-order[d];
           }
           auto cp = controlPoints.get(multiIndexControlNet);
@@ -304,13 +316,109 @@ namespace Dune
         return glob;
       }
 
-      /** \brief evaluates the basis Functions at the given local coordinates
+      const JacobianTransposed jacobianTransposed(const LocalCoordinate &local)
+      {
+        for (int i=0; i<dim;++i)
+          if (local[i]<0 || local[i]>1)
+            DUNE_THROW(RangeError, "Local coordinates have to be in [0,1]^dim!");
+
+        JacobianTransposed result;
+        const auto& knotSpans = patchData_->getKnots();
+        const auto& controlPoints = patchData_->getControlPoints();
+        const auto& order = patchData_->getOrder();
+        const auto& basis = this->getBasis(local);
+
+        //const auto& basis = (basis_.empty())?this->getBasis(local):basis_;
+        const auto& derBasis = this->getDerBasis();
+        std::array<unsigned int,mydimension> multiIndexBasisfucntion, multiIndexControlNet;
+        std::array<unsigned int,mydimension> dimsize;
+        std::array<unsigned int,mydimension> cornerIdx;
+        for (unsigned int d=0; d<mydimension; ++d)
+        {
+          dimsize[d] = order[d]+1;
+          cornerIdx[d] = corner_[d]-knotSpans[d].begin();
+        }
+        /*Index net for valid basis functions*/
+        auto basisFunctionNet = MultiDimensionNet<mydimension,coorddimension>(dimsize);
+        /*Iterate the matrix row*/
+        for (unsigned int r=0; r<mydimension; ++r)
+        {
+          std::fill(result[r].begin(),result[r].end(),0);
+          /*Iterate the non-vanishing basis function grid*/
+          for (unsigned int i=0; i<basisFunctionNet.directSize(); ++i)
+          {
+            multiIndexBasisfucntion =  basisFunctionNet.directToMultiIndex(i);
+            double product = 1;
+            for (unsigned int d=0; d<mydimension; ++d)
+            {
+              double temp = (d == r)? derBasis[d][multiIndexBasisfucntion[d]]:basis[d][multiIndexBasisfucntion[d]][order[d]];
+              product *= temp;
+              multiIndexControlNet[d] = multiIndexBasisfucntion[d]+cornerIdx[d]-order[d];
+            }
+            auto cp = controlPoints.get(multiIndexControlNet);
+            /* Fill the matrix row */
+            for (unsigned int k=0; k<coorddimension; ++k)
+            {
+              result[r][k] += cp[k]*product;
+            }
+
+          }
+        }
+        return result;
+
+      }
+
+      /** \brief obtain the integration element
+       *
+       *  If the Jacobian of the mapping is denoted by $J(x)$, the integration
+       *  integration element \f$\mu(x)\f$ is given by
+       *  \f[ \mu(x) = \sqrt{|\det (J^T(x) J(x))|}.\f]
+       *
+       *  \param[in]  local  local coordinate to evaluate the integration element in
+       *
+       *  \returns the integration element \f$\mu(x)\f$.
+       *
+       */
+      ctype integrationElement(const LocalCoordinate &local) const
+      {
+        return MatrixHelper::template sqrtDetAAT< mydimension,coorddimension>(jacobianTransposed(local));
+      }
+
+      /** \brief evaluates the non-zero derivatives of basis functions, order+1 for each dimension
        *
        *  \param[in] local local coordinates for each dimension
        */
-      const std::array<std::vector<double>,dim > & getBasis (const LocalCoordinate& local)
+      const std::array<std::vector<double>,mydimension> getDerBasis()
       {
-        for (int i=0; i<dim;++i)
+        const auto & order = patchData_->getOrder();
+        const auto & knotSpans = patchData_->getKnots();
+        std::array<std::vector<double>, mydimension> der;
+
+        /*redo basis calculation or not? */
+
+        for (unsigned int d=0; d<mydimension; ++d)
+        {
+          der[d].resize(order[d]+1);
+          /*The first one , what if it doesn't exist*/
+          der[d][0] = -order[d]*basis_[d][0][order[d]-1]/basis_[d][order[d]][0];
+          for (unsigned int j=1; j<order[d]; ++j)
+          {
+            der[d][j] = order[d]*(basis_[d][j-1][order[d]-1]/basis_[d][order[d]][j-1]-basis_[d][j][order[d]-1]/basis_[d][order[d]][j]);
+          }
+          /*The last one, what if it doesn't exist */
+          der[d][order[d]] = order[d]*basis_[d][order[d]-1][order[d]-1]/basis_[d][order[d]][order[d]-1];
+        }
+        return der;
+      }
+
+
+      /** \brief evaluates the non-zero basis Functions at the given local coordinates, order+1 for each dimension
+       *
+       *  \param[in] local local coordinates for each dimension
+       */
+      const std::array<std::vector<std::vector<double>>,mydimension > & getBasis (const LocalCoordinate& local)
+      {
+        for (int i=0; i<dim; ++i)
           if (local[i]<0 || local[i]>1)
             DUNE_THROW(RangeError, "Local coordinates have to be in [0,1]^dim!");
 
@@ -326,19 +434,26 @@ namespace Dune
           std::vector<double> left, right;
           left.resize(order[d]+1);
           right.resize(order[d]+1);
-          basis_[d][0] = 1;
+          basis_[d][0].resize(order[d]+1);
+          basis_[d][0][0] = 1;
+          /*Build a squared table to store the knot differences as well to make derivative computation easier */
           for (int o=1; o<=order[d]; ++o)
           {
+            basis_[d][o].resize(order[d]+1);
             left[o] = loc-*(corner_[d]+1-o);
             right[o] = *(corner_[d]+o)-loc;
             double saved = 0;
             for (int r=0; r<o; ++r)
             {
-                double temp = basis_[d][r]/(right[r+1]+left[o-r]);
-                basis_[d][r] = saved+right[r+1]*temp;
-                saved = left[o-r]*temp;
+              /*Lower triangle - knot differences */
+              basis_[d][o][r] = right[r+1]+left[o-r];
+              double temp = basis_[d][r][o-1]/basis_[d][o][r];
+
+              /*Upper triangle - basis function*/
+              basis_[d][r][o] = saved+right[r+1]*temp;
+              saved = left[o-r]*temp;
             }
-            basis_[d][o] = saved;
+            basis_[d][o][o] = saved;
           }
         }
 
@@ -351,9 +466,43 @@ namespace Dune
 
         std::array<std::vector<double>::const_iterator,dim> corner_;
 
-        std::array<std::vector<double>,dim> basis_;
+        std::array<std::vector<std::vector<double>>,dim> basis_;
 
     };
+//
+//    class Iter
+//    {
+//    public:
+//      Iter (const IntVector* p_vec, int pos)
+//          : _pos( pos )
+//          , _p_vec( p_vec )
+//      { }
+//
+//      // these three methods form the basis of an iterator for use with
+//      // a range-based for loop
+//      bool
+//      operator!= (const Iter& other) const
+//      {
+//          return _pos != other._pos;
+//      }
+//
+//      // this method must be defined after the definition of IntVector
+//      // since it needs to use it
+//      int operator* () const;
+//
+//      const Iter& operator++ ()
+//      {
+//          ++_pos;
+//          // although not strictly necessary for a range-based for loop
+//          // following the normal convention of returning a value from
+//          // operator++ is a good idea.
+//          return *this;
+//      }
+//
+//      private:
+//      int _pos;
+//      const IntVector *_p_vec;
+//    };
 
     /** \brief Class where the B-Spline geometry can work on  */
     template <int dim, int dimworld>
