0f1db6ef13f7cc6bb6eeb3360bbb78ed223feed2
henrij22 <96132706+henrij22@users.noreply.github.com>
2023-03-17 17:55:36 +0100

    Mirror full interface of NURBSGeometry

--- dune/iga/test/dune-iga.cc
+++ dune/iga/test/dune-iga.cc
@@ -45,7 +45,7 @@ struct Compare {
     return std::ranges::lexicographical_compare(std::ranges::join_view(lhs), std::ranges::join_view(rhs));
   };
 };
-#if 0
+
 auto checkUniqueEdges(const auto& gridView) {
   TestSuite t;
 
@@ -1005,7 +1005,7 @@ auto testPlate() {
   t.subTest(checkUniqueEdges(gridView));
   return t;
 }
-#endif
+
 
 auto testPatchGeometryCurve() {
   TestSuite t;
@@ -1035,17 +1035,49 @@ auto testPatchGeometryCurve() {
   // Make Geometry
   NURBSPatchGeometry<dim, dimworld> geometry(std::make_shared<Dune::IGA::NURBSPatchData<dim, dimworld>>(patchData));
 
-  auto p0 = geometry.global(Dune::FieldVector<double, 1>{0.0});
+  auto p0 = geometry.global({0.0});
   t.check(Dune::FloatCmp::eq(p0, {-4, -4}));
 
-  auto p1 = geometry.global(Dune::FieldVector<double, 1>{0.5});
+  auto p1 = geometry.global({0.5});
   t.check(Dune::FloatCmp::eq(p1, {-1.32, 0.72}));
 
-  auto p2 = geometry.global(Dune::FieldVector<double, 1>{1});
+  auto p2 = geometry.global({1});
   t.check(Dune::FloatCmp::eq(p2, {4, 4}));
 
-  auto p3 = geometry.global(Dune::FieldVector<double, 1>{0.25});
+  auto p3 = geometry.global({0.25});
   t.check(Dune::FloatCmp::eq(p3, {-2.7607655502392343, 0.4688995215311005}));
+
+  // Test Operator ()
+  auto p4 = geometry({0.4});
+  t.check(Dune::FloatCmp::eq(p4, {-1.9854368932038828, 0.7669902912621357}));
+
+  // Check derivative
+  auto jc0 = geometry.jacobianTransposed({0});
+  t.check(Dune::FloatCmp::eq(jc0[0], {7.5, 51}));
+
+  auto jc1 = geometry.jacobianTransposed({0.5});
+  t.check(Dune::FloatCmp::eq(jc1[0], {7.4496, -0.9216}));
+
+  // Check local function
+  auto u0 = geometry.local({-4, -4});
+  t.check(Dune::FloatCmp::eq(u0, {0}));
+
+  auto u1 = geometry.local({-1.32, 0.72});
+  t.check(Dune::FloatCmp::eq(u1, {0.5}));
+
+  // geomdl reports 13.230641820866644 for the length of the curve. The volume function approaches this value,
+  // if you use a lot of gauß-points
+  auto len = geometry.volume();
+
+  // Check corners
+  t.check(geometry.corners() == 2);
+  std::array<FieldVector<double, 2>, 2> expectedCorners{{
+      FieldVector<double, 2>{-4, -4},
+      FieldVector<double, 2>{4, 4}
+  }};
+  for (int i = 0; i < 2; ++i)
+    t.check(Dune::FloatCmp::eq(geometry.corner(i), expectedCorners[i]));
+
 
   return t;
 }
@@ -1087,10 +1119,39 @@ auto testPatchGeometrySurface() {
   auto p3 = geometry.global(FieldVector<double, 2>{0, 1});
   t.check(Dune::FloatCmp::eq(p3, {2, 0, 2}));
 
+  // Check derivative
+  auto jc1 = geometry.jacobianTransposed({0.5, 0.5});
+  t.check(Dune::FloatCmp::eq(jc1[0], {0.0, 2.0, 0.5}));
+  t.check(Dune::FloatCmp::eq(jc1[1], {2.0, 0.0, 0.5}));
+
+  // Check local function
+  auto u1 = geometry.local({1.0, 1.0, 0.75});
+  t.check(Dune::FloatCmp::eq(u1, {0.5, 0.5}));
+
+  auto u2 = geometry.local({2, 0, 2});
+  t.check(Dune::FloatCmp::eq(u2, {0, 1}));
+
+  // Check corners
+  t.check(geometry.corners() == 4);
+
+  std::array<FieldVector<double, 3>, 4> expectedCorners{{
+      FieldVector<double, 3>{0, 0, 1},
+      FieldVector<double, 3>{0, 2, 1},
+      FieldVector<double, 3>{2, 0, 2},
+      FieldVector<double, 3>{2, 2, 2}
+  }};
+  for (int i = 0; i < 4; ++i)
+    t.check(Dune::FloatCmp::eq(geometry.corner(i), expectedCorners[i]));
+
+  // Check domain
+  t.check(Dune::FloatCmp::eq(geometry.domain()[0][0], 0.0));
+  t.check(Dune::FloatCmp::eq(geometry.domain()[0][1], 1.0));
+
+  // Check domain midpoint
+  t.check(Dune::FloatCmp::eq(geometry.domainMidPoint()[0], 0.5));
+
   return t;
 }
-
-
 
 int main(int argc, char** argv) try {
   // Initialize MPI, if necessary
@@ -1100,16 +1161,16 @@ int main(int argc, char** argv) try {
   t.subTest(testPatchGeometryCurve());
   t.subTest(testPatchGeometrySurface());
 
-//  t.subTest(test3DGrid());
-//  t.subTest(testNURBSGridCurve());
-//  t.subTest(testPlate());
-//  testNurbsGridCylinder();
-//  t.subTest(testTorusGeometry());
-//
-//  t.subTest(testNurbsBasis());
-//
-//  gridCheck();
-//  t.subTest(testBsplineBasisFunctions());
+  t.subTest(test3DGrid());
+  t.subTest(testNURBSGridCurve());
+  t.subTest(testPlate());
+  testNurbsGridCylinder();
+  t.subTest(testTorusGeometry());
+
+  t.subTest(testNurbsBasis());
+
+  gridCheck();
+  t.subTest(testBsplineBasisFunctions());
   return 0;
 } catch (Dune::Exception& e) {
   std::cerr << "Dune reported error: " << e << std::endl;
