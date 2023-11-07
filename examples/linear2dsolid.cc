214e753365248aba79486e860c25d667199de9d7
Henrik Jakob <96132706+henrij22@users.noreply.github.com>
2023-11-07 11:11:17 +0100

    Fix examples (#33) (#40)

--- examples/linear2dsolid.cc
+++ examples/linear2dsolid.cc
@@ -3,10 +3,9 @@
 
 #include <config.h>
 
-#include "kirchhoffplate.hh"
-
 #include <ikarus/assembler/simpleAssemblers.hh>
 #include <ikarus/finiteElements/feRequirements.hh>
+#include <ikarus/finiteElements/mechanics/linearElastic.hh>
 #include <ikarus/linearAlgebra/dirichletValues.hh>
 #include <ikarus/linearAlgebra/nonLinearOperator.hh>
 #include <ikarus/solver/linearSolver/linearSolver.hh>
@@ -14,23 +13,25 @@
 #include <ikarus/utils/init.hh>
 #include <ikarus/utils/observer/controlVTKWriter.hh>
 
-#include "dune/iga/io/ibra/ibrareader.hh"
-#include "dune/iga/io/igadatacollector.hh"
-#include "dune/iga/nurbsgrid.hh"
-#include "dune/iga/utils/igahelpers.hh"
 #include <dune/common/parametertreeparser.hh>
+#include <dune/iga/io/ibra/ibraReader.hh>
+#include <dune/iga/io/igadatacollector.hh>
+#include <dune/iga/nurbsbasis.hh>
+#include <dune/iga/nurbsgrid.hh>
+#include <dune/iga/utils/igahelpers.hh>
 #include <dune/vtk/vtkwriter.hh>
+#include <dune/vtk/writers/unstructuredgridwriter.hh>
 
 int main(int argc, char **argv) {
-  std::cout << "ALUGRID_VERBOSITY_LEVEL0: " << getenv("ALUGRID_VERBOSITY_LEVEL") << std::endl;
   Ikarus::init(argc, argv);
 
   constexpr int gridDim  = 2;
   constexpr int worldDim = 2;
+  double lambdaLoad      = 1;
 
   /// Read Parameter
   Dune::ParameterTree parameterSet;
-  Dune::ParameterTreeParser::readINITree(argv[1], parameterSet);
+  Dune::ParameterTreeParser::readINITree("auxiliaryFiles/linear2dsolid.parset", parameterSet);
 
   const Dune::ParameterTree &gridParameters        = parameterSet.sub("GridParameters");
   const Dune::ParameterTree &materialParameters    = parameterSet.sub("MaterialParameters");
@@ -41,58 +42,72 @@ int main(int argc, char **argv) {
   const auto u_degreeElevate = gridParameters.get<int>("u_degreeElevate");
   const auto v_degreeElevate = gridParameters.get<int>("v_degreeElevate");
   const auto globalRefine    = gridParameters.get<int>("globalRefine");
-  const auto u_refine        = gridParameters.get<int>("u_refine");
-  const auto v_refine        = gridParameters.get<int>("v_refine");
 
-  const auto E   = materialParameters.get<double>("E");
-  const auto nu  = materialParameters.get<double>("nu");
-  const auto thk = materialParameters.get<double>("thk");
-
-  double lambdaLoad = 1 * std::pow(thk, 3);
+  const auto E  = materialParameters.get<double>("E");
+  const auto nu = materialParameters.get<double>("nu");
 
   const int subsample = postProcessParameters.get<int>("subsample");
-  std::cout << "ALUGRID_VERBOSITY_LEVEL1: " << getenv("ALUGRID_VERBOSITY_LEVEL") << std::endl;
-  // Log the Parameters
-  spdlog::info(
-      "Filename: {} \n The following parameters were used: \nMaterial: E {}, nu {} \nRefinements: global {}, u {}, v "
-      "{}",
-      gridFileName, E, nu, globalRefine, u_refine, v_refine);
-  std::cout << "ALUGRID_VERBOSITY_LEVEL2: " << getenv("ALUGRID_VERBOSITY_LEVEL") << std::endl;
+
   using Grid     = Dune::IGA::NURBSGrid<gridDim, worldDim>;
   using GridView = Dune::IGA::NURBSGrid<gridDim, worldDim>::LeafGridView;
-  std::cout << "ALUGRID_VERBOSITY_LEVEL2: " << getenv("ALUGRID_VERBOSITY_LEVEL") << std::endl;
+
   std::shared_ptr<Grid> grid = Dune::IGA::IbraReader<gridDim, worldDim>::read(
       "auxiliaryFiles/" + gridFileName, trimGrid, {u_degreeElevate, v_degreeElevate});
-
-  std::cout << "ALUGRID_VERBOSITY_LEVEL4: " << getenv("ALUGRID_VERBOSITY_LEVEL") << std::endl;
   grid->globalRefine(globalRefine);
+
   GridView gridView = grid->leafGridView();
-  std::cout << "ALUGRID_VERBOSITY_LEVEL5: " << getenv("ALUGRID_VERBOSITY_LEVEL") << std::endl;
-  const auto &patchData = grid->getPatch().getPatchData();
-  spdlog::info("Degree: u {}, v {}", patchData->degree[0], patchData->degree[1]);
 
   using namespace Dune::Functions::BasisFactory;
-  auto basis = Ikarus::makeBasis(gridView, gridView.impl().getPreBasis());
+  auto basis = Ikarus::makeBasis(gridView, power<gridDim>(nurbs(), FlatInterleaved()));
 
   // Clamp the left boundary
   Ikarus::DirichletValues dirichletValues(basis.flat());
 
   dirichletValues.fixDOFs([](auto &basis_, auto &dirichletFlags) {
     Dune::Functions::forEachUntrimmedBoundaryDOF(basis_, [&](auto &&localIndex, auto &&localView, auto &&intersection) {
-      dirichletFlags[localView.index(localIndex)] = true;
+      if (std::abs(intersection.geometry().center()[0]) < 1e-8) dirichletFlags[localView.index(localIndex)] = true;
     });
   });
 
-  /// Declare a vector "fes" of kirchhoff plate 2D elements
-  using LinearElasticType = Ikarus::KirchhoffPlate<decltype(basis)>;
+  /// Declare a vector "fes" of linear elastic 2D planar solid elements
+  using LinearElasticType = Ikarus::LinearElastic<decltype(basis)>;
   std::vector<LinearElasticType> fes;
+
+  /// function for volume load- here: returns zero
+  auto volumeLoad = [](auto &globalCoord, auto &lamb) {
+    Eigen::Vector2d fext;
+    fext.setZero();
+    return fext;
+  };
+
+  /// neumann boundary load in horizontal direction
+  auto neumannBoundaryLoad = [&](auto &globalCoord, auto &lamb) {
+    Eigen::Vector2d F = Eigen::Vector2d::Zero();
+    F[0]              = lamb;
+    return F;
+  };
+
+  const auto &indexSet = gridView.indexSet();
+
+  /// Flagging the vertices on which neumann load is applied as true
+  Dune::BitSetVector<1> neumannVertices(gridView.size(2), false);
+  auto neumannPredicate = [](auto &vertex) -> bool { return std::isgreaterequal(vertex[0], 10 - 1e-8); };
+  for (auto &&vertex : vertices(gridView)) {
+    auto coords                             = vertex.geometry().corner(0);
+    neumannVertices[indexSet.index(vertex)] = neumannPredicate(coords);
+  }
+
+  BoundaryPatch<decltype(gridView)> neumannBoundary(gridView);
+
+  // create property for insertion by vertex vector
+  BoundaryPatchEnclosingVerticesPropertyTrimmed<GridView, 1> prop(gridView, neumannVertices);
+  neumannBoundary.insertFacesByProperty(prop);
 
   /// Add the linear elastic 2D planar solid elements to the vector "fes"
   for (auto &element : elements(gridView)) {
     auto localView = basis.flat().localView();
-    fes.emplace_back(basis, element, E, nu, thk);
+    fes.emplace_back(basis, element, E, nu, volumeLoad, &neumannBoundary, neumannBoundaryLoad);
   }
-
   /// Create a sparse assembler
   auto sparseAssembler = Ikarus::SparseFlatAssembler(fes, dirichletValues);
 
@@ -113,37 +128,32 @@ int main(int argc, char **argv) {
 
   Eigen::VectorXd D_Glob = Eigen::VectorXd::Zero(basis.flat().size());
 
-  /// Create a non-linear operator
-  auto nonLinOp = Ikarus::NonLinearOperator(Ikarus::linearAlgebraFunctions(residualFunction, KFunction),
+  auto nonLinOp = Ikarus::NonLinearOperator(Ikarus::functions(residualFunction, KFunction),
                                             Ikarus::parameter(D_Glob, lambdaLoad));
 
   const auto &K    = nonLinOp.derivative();
   const auto &Fext = nonLinOp.value();
 
   /// solve the linear system
-  auto linSolver = Ikarus::ILinearSolver<double>(Ikarus::SolverTypeTag::sd_CholmodSupernodalLLT);
+  auto linSolver = Ikarus::LinearSolver(Ikarus::SolverTypeTag::sd_CholmodSupernodalLLT);
 
   linSolver.compute(K);
   linSolver.solve(D_Glob, -Fext);
 
   /// Postprocess
   auto dispGlobalFunc
-      = Dune::Functions::makeDiscreteGlobalBasisFunction<Dune::FieldVector<double, 1>>(basis.flat(), D_Glob);
+      = Dune::Functions::makeDiscreteGlobalBasisFunction<Dune::FieldVector<double, 2>>(basis.flat(), D_Glob);
 
   auto forceGlobalFunc
-      = Dune::Functions::makeDiscreteGlobalBasisFunction<Dune::FieldVector<double, 1>>(basis.flat(), Fext);
+      = Dune::Functions::makeDiscreteGlobalBasisFunction<Dune::FieldVector<double, 2>>(basis.flat(), Fext);
 
   Dune::Vtk::DiscontinuousIgaDataCollector dataCollector(gridView, subsample);
-  Dune::VtkUnstructuredGridWriter vtkWriter(dataCollector, Dune::Vtk::FormatTypes::ASCII);
+  Dune::Vtk::UnstructuredGridWriter<GridView, decltype(dataCollector)> vtkWriter(dataCollector,
+                                                                                 Dune::Vtk::FormatTypes::ASCII);
 
-  vtkWriter.addPointData(dispGlobalFunc, Dune::VTK::FieldInfo("displacement", Dune::VTK::FieldInfo::Type::scalar, 1));
+  vtkWriter.addPointData(dispGlobalFunc, Dune::VTK::FieldInfo("displacement", Dune::VTK::FieldInfo::Type::vector, 2));
   vtkWriter.addPointData(forceGlobalFunc,
-                         Dune::VTK::FieldInfo("external force", Dune::VTK::FieldInfo::Type::scalar, 1));
-
-  double totalForce = 0.0;
-  for (auto &f : Fext)
-    totalForce += f;
-  std::cout << "Total Force: " << totalForce << std::endl;
+                         Dune::VTK::FieldInfo("external force", Dune::VTK::FieldInfo::Type::vector, 2));
 
   vtkWriter.write(gridFileName);
 
