c6464951770a17e989604e4de6a85650da2d9d97
AlexanderMueller <rathet@gmail.com>
2023-11-08 10:14:25 +0100

    remove examples using ikarus
    this had to be done to solve a circular dependencies when configuring ikarus

---
+++ sandbox/src/sandbox.cc
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2023 The dune-iga developers mueller@ibb.uni-stuttgart.de
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+#include "config.h"
+
+#include "dune/iga/io/ibra/ibrareader.hh"
+#include "dune/iga/io/igadatacollector.hh"
+#include "dune/iga/nurbsbasis.hh"
+#include "dune/iga/nurbsgrid.hh"
+#include "dune/iga/utils/igahelpers.hh"
+#include <dune/common/parametertreeparser.hh>
+
+int main(int argc, char **argv) {
+  std::cout << "Hello this is dune-iga" << std::endl;
+  return 0;
+}
